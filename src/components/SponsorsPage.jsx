a5d02c358914e32e9d2dcf8d833b8bf506d96eb0
Duan, Daniel Lee <dduan6@gatech.edu>
2022-03-09 15:22:12 -0500

    setup sponsors page

--- src/components/SponsorsPage.jsx
+++ src/components/SponsorsPage.jsx
@@ -24,6 +24,7 @@ export const SponsorsPage = () => {
         <div>
             <Navigation/>
             <SponsorList/>
+            
             <Footer data={sponsorsPageData.Contact} />
         </div>
     );
