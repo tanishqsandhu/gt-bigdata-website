5f7933f06df70ba7dcd64667f390a088441922d2
Abhinav <abhinav.sattiraju@gmail.com>
2022-04-01 14:52:49 -0400

    website progress

--- src/components/projRoles.jsx
+++ src/components/projRoles.jsx
@@ -1,31 +1,57 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function RoleCard(props) {
 
-    const [hover, setHover] = useState(false);
+    const [hover, setHover] = useState({
+        hover: false,
+        load: false
+    });
+
+    useEffect(() => {
+        setHover({
+            hover: false,
+            load: false
+        })
+    },[])
 
     function setClass() {
-        if (hover) {
+        if (hover.hover) {
             return "shade";
-        } else {
+        } else if (hover.load) {
             return "light";
         }
+        return "";
     }
 
     function visibility() {
-        if (hover) {
+        if (hover.hover) {
             return "desc fade-in"
-        } else {
+        } else if (hover.load) {
             return "desc fade-out"
         }
+        return "default";
     }
 
+    function changeState() {
+        if (hover.hover) {
+            setHover({
+                hover: false,
+                load: true
+            });
+        } else {
+            setHover({
+                hover: true,
+                load: true
+            })
+        }
+    }
 
     return (
         <div>
             <div className="role-card">
-                <div className="title">
-                    <div className={setClass()} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                <div className="role-title">
+                    <div className={setClass()} onClick={changeState}>
                         {props.data.title}
                     </div>
                 </div>
@@ -42,15 +68,21 @@ function RoleCard(props) {
 export const ProjRoles = (props) => {
 
     return (
-        <div className="container">
-            <h1 style={{textAlign: 'center',color: '#6372ff', margin: '2em 1em 1em 1em'}}>Project roles</h1>
-            <h3 >Learn more about each of the 3 roles every project has! 
-                Each category is coordinated with workshops that help
-                students develop production-level code. <span style={{'color':'#6372ff'}}> Hover over the buttons to learn more!</span>
-            </h3>
-            <RoleCard data={props.data.analysisRole}></RoleCard>
-            <RoleCard data={props.data.platformRole}></RoleCard>
-            <RoleCard data={props.data.datavizRole}></RoleCard>
+        <div className=" project-roles container">
+            <div className="row">
+                <div className="img">
+                    <img src="../img/project_team.png" alt="project" />
+                </div>
+                <h1 style={{textAlign: 'center',color: '#6372ff', margin: '2em 1em 2em 1em'}}>Project roles</h1>
+                <h3 >Learn more about each of the 3 roles every project has! 
+                    Each category is coordinated with workshops that help
+                    students develop production-level code. <span style={{'color':'#6372ff'}}> Hover over the buttons to learn more!</span>
+                </h3>
+                <RoleCard data={props.data.analysisRole}></RoleCard>
+                <RoleCard data={props.data.platformRole}></RoleCard>
+                <RoleCard data={props.data.datavizRole}></RoleCard>
+
+            </div>
         </div>
     );
 }
