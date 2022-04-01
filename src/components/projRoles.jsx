3eff57fe937e76542a990376d69bdaca1d5abf7d
Abhinav <abhinav.sattiraju@gmail.com>
2022-04-01 00:12:47 -0400

    finished project role styling

--- src/components/projRoles.jsx
+++ src/components/projRoles.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
 function RoleCard(props) {
+
+    const [hover, setHover] = useState(false);
+
+    function setClass() {
+        if (hover) {
+            return "shade";
+        } else {
+            return "light";
+        }
+    }
+
+    function visibility() {
+        if (hover) {
+            return "desc fade-in"
+        } else {
+            return "desc fade-out"
+        }
+    }
+
+
     return (
-        <div className="container">
-            <div className="row role-card">
-                <h3>
-                    <span>
+        <div>
+            <div className="role-card">
+                <div className="title">
+                    <div className={setClass()} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                         {props.data.title}
-                    </span>
-                </h3>
-                <div>{props.data.desc}</div>
+                    </div>
+                </div>
+                <div className={visibility()}>
+                    {props.data.desc}
+                </div>
             </div>
         </div>
     );
@@ -16,12 +40,13 @@ function RoleCard(props) {
 
 
 export const ProjRoles = (props) => {
+
     return (
         <div className="container">
             <h1 style={{textAlign: 'center',color: '#6372ff', margin: '2em 1em 1em 1em'}}>Project roles</h1>
             <h3 >Learn more about each of the 3 roles every project has! 
                 Each category is coordinated with workshops that help
-                students develop production-level code.
+                students develop production-level code. <span style={{'color':'#6372ff'}}> Hover over the buttons to learn more!</span>
             </h3>
             <RoleCard data={props.data.analysisRole}></RoleCard>
             <RoleCard data={props.data.platformRole}></RoleCard>
