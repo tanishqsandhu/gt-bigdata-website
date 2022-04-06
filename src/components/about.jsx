11325e2f3ef1017816f533a502b7b19cfc920c2c
Abhinav <abhinav.sattiraju@gmail.com>
2022-04-06 19:54:03 -0400

    further styling improvements

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,14 +5,15 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12">
             {" "}
-            {/* <img src="img/about.jpg" className="img-responsive" alt="" />{" "} */}
           </div>
           <div className="col-xs-12">
+            <h2>About Us</h2>
             <div className="about-text">
-              <h2>About Us</h2>
-              <div className="keywords" style={{color: "#6372ff"}}>{props.data ? props.data.keywords : 'loading...'}</div>
-              <p className="">{props.data ? props.data.paragraph1 : "loading..."}</p>
-              <p className="">{props.data ? props.data.paragraph2 : "loading..."}</p>
+              <div>
+                <div className="keywords" style={{color: "#6372ff"}}>{props.data ? props.data.keywords : 'loading...'}</div>
+                <p className="">{props.data ? props.data.paragraph1 : "loading..."}</p>
+                <p className="">{props.data ? props.data.paragraph2 : "loading..."}</p>
+              </div>
             </div>
           </div>
         </div>
