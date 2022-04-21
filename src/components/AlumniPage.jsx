3fe3a256206c947f7bc96dc38ae0edf6fb21e7ec
Abhinav <abhinav.sattiraju@gmail.com>
2022-04-21 08:44:45 -0400

    json, basic outline done for alumni page

--- src/components/AlumniPage.jsx
+++ src/components/AlumniPage.jsx
@@ -1,38 +1,68 @@
 import { Navigation } from "./navigation";
 import { Footer } from "./footer";
 import JsonData from "../data/data.json";
+import { useState } from "react";
+
+//add on if necessary - past years
+let years = ["2021", "2020", "2019"];
+
 export const AlumniPage = (props) => {
+
+  const [year, setYear] = useState("2021");
+
+  const YearButton = (props) => {
+    return (
+      // <label className="btn btn-primary">
+      //   <input
+      //     type="radio"
+      //     name={props.year}
+      //     value="android"
+      //     autoComplete="off"
+
+      //     />{" "}
+      //   {props.year}
+      // </label>
+      <button className='btn btn-custom page-scroll' onClick={setY}>
+        {props.year}
+      </button>
+    )
+  };
+
+  function setY(e) {
+    setYear(e.target.innerText);
+    console.log(JsonData.Alumni[2019]);
+  }
+
   return (
     <div>
-    <div id='team' className='text-center'>
+    <div id='alumni' className='text-center'>
       <Navigation />
       <div className='col-md-8 col-md-offset-2 section-title'>
           <h2>Alumni</h2>
           <p>
             Check out our alumni from past years!
           </p>
+      </div>
+      <div className="container">
+        <div className="btn-group">
+          {
+            years.map(num => (
+              <YearButton year={num}/>
+            ))
+          }
         </div>
-        <div class="btn-group btn-group-toggle" data-toggle="buttons">
-            <label class="btn btn-secondary active">
-                <input type="radio" name="options" id="option1" autocomplete="off" checked/> 2021
-            </label>
-            <label class="btn btn-secondary">
-                <input type="radio" name="options" id="option2" autocomplete="off"/> 2020
-            </label>
-            <label class="btn btn-secondary">
-                <input type="radio" name="options" id="option3" autocomplete="off"/> 2019
-            </label>
-        </div>
-      
-        <div className="container">
-        <div id='row' className="cardrow ">
-            {JsonData.Team
-            ? JsonData.Team.map((d, i) => (
+        <h1 className="year" style={{"margin": "1em 0em 1em 0em"}}>
+          {year}
+        </h1>
+
+        <div id='row' className="cardrow">
+            {JsonData.Alumni[year]
+            ? JsonData.Alumni[year].map((d, i) => (
               <div className='card col-xs-4'>
-                <img src={d.img} />
+                <img src={d.img} alt=""/>
                 <div className='caption'>
                     <h4>{d.name}</h4>
-                <div class="social-links">
+                <div className="social-links">
                   {d.linkedin ? <a href={d.linkedin} className="linkedin"><i class="fa fa-linkedin"></i></a> : ""}
                   {d.instagram ? <a href={d.instagram} className="insta"><i class="fa fa-instagram"></i></a> : ""}
                   {d.twitter ? <a href={d.twitter} className="twitter"><i class="fa fa-twitter"></i></a> : ""}
@@ -40,9 +70,9 @@ export const AlumniPage = (props) => {
                 </div>
               </div>
               ))
-            : 'hey'}
-          </div>
-          </div>
+          : 'hey'}
+        </div>
+      </div>
         
       
       
