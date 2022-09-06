02c1d67a6f38aa24385801d2f6f0b921d70de95f
Duan, Daniel Lee <dduan6@gatech.edu>
2022-09-05 21:38:58 -0400

    Updated sponsor boxes
    Updated sponsor boxes into separate boxes for past and current sponsors, also fixed styling of sponsor page title to match rest of website

--- src/components/SponsorsPage.jsx
+++ src/components/SponsorsPage.jsx
@@ -10,12 +10,16 @@ import { PastSponsors } from "./past";
 export const SponsorsPage = () => {
     return (
         <div>
+        <div id='sponsors' className="text-center">
+                <div className='col-md-8 col-md-offset-2 section-title'>
+                    <h2>Sponsors</h2>
+                    <p>Thanks for making what we do possible!</p>
+                </div>
             <Navigation />
-            
             <div className="container container-table">
                 <div className="row vertical-center-row">      
-                    <h2 style={{textAlign: 'center', margin: '3em 1em 1em 1em'}}>Our Sponsors</h2>
-                    <div className="keywords" style={{textAlign: 'center', margin: '1em 1em 1em 1em', color: "#6372ff", fontSize: 25}}>Thanks for making what we do possible!</div>
+                    {/* <h2 style={{textAlign: 'center', margin: '3em 1em 1em 1em'}}>Our Sponsors</h2>
+                    <div className="keywords" style={{textAlign: 'center', margin: '1em 1em 1em 1em', color: "#6372ff", fontSize: 25}}>Thanks for making what we do possible!</div> */}
                     <div className="sponsorButtons">
                         <a href="" class="btn btn-sponsors btn-custom">View Information</a>
                         <a href="" class="btn btn-sponsors btn-custom">Become a Sponsor</a>
@@ -23,11 +27,12 @@ export const SponsorsPage = () => {
                 </div>
             </div>
             <DiamondSponsors data={JsonData.Sponsors.DiamondSponsors}/>
-            {/* <PlatinumSponsors data={JsonData.Sponsors.PlatinumSponsors}/>
-            <PastSponsors data={JsonData.Sponsors.PastSponsors}/> */}
+            {/* <PlatinumSponsors data={JsonData.Sponsors.PlatinumSponsors}/> */}
+            <PastSponsors data={JsonData.Sponsors.PastSponsors}/>
 
             <Footer data={JsonData.Contact} />
-        </div>     
+        </div>   
+        </div>  
     );
 };
 
